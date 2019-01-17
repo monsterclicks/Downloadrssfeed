46d95279a360ee4913d1fb3b6b54f29fe37b7abb
TonyDaGroot <tony.nguyen@sendviper.io>
2019-01-17 16:13:07 -0600

    update

--- index.php
+++ index.php
@@ -17,6 +17,17 @@ var_dump($data);
 echo "=================<pre>";
 
  for($i=1;$i<=$totalc;$i++){
+   $find_ifequalvoltn_cat_a = strstr($template,"[IFEQUALVOLTN_CAT");
+   $find_ifequalvoltn_cat = strstr($find_ifequalvoltn_cat_a," ",true);
+   if($strpos($template,$find_ifequalvoltn_cat) != FALSE){
+     $parts = explode("_",$find_ifequalvoltn_cat);
+     $var = "[VOLTN_CATEGORY_{$i}]";
+     $comparison = $parts[2];
+     $result = $parts[3];
+     $else = $parts[4];
+     $replacement = ($var == $data['data']['recs'][$i]['props']['category'])?$result:$else;
+     $template = str_replace($find_ifequalvoltn_cat,$replacement,$template);
+   }
    //var_dump($data['data']['recs'][$i]['props']['title']);
     echo "[VOLTN_TITLE_{$i}] - ". $data['data']['recs'][$i]['props']['title']."</br>";
    $template = str_replace("[VOLTN_TITLE_{$i}]",$data['data']['recs'][$i]['props']['title'],$template);
@@ -25,9 +36,28 @@ echo "=================<pre>";
    $template = str_replace("[VOLTN_THUMBNAIL_".$i."]",$data['data']['recs'][$i]['props']['thumbnail'],$template);
    $template = str_replace("[VOLTN_URL_".$i."]",$data['data']['recs'][$i]['props']['url'],$template);
    $template = str_replace("[VOLTN_ID_".$i."]",$data['data']['recs'][$i]['id'],$template);
+
+
  }
 echo "=====================</br>";
 
+function if_equal_tag($string){
+  $parts = explode("_",$string);
+  $part_a = $parts[0];
+  $part_b = $parts[1];
+  $var = $part[2];
+  $comparison = $part[3];
+  $result = $part[4];
+  $else = $part[5];
+  if (strpos($var,$comparison) != FALSE){
+    return $res;
+  }
+  else {
+    return $else;
+  }
+}
+
+$template
 echo $template;
 
 
