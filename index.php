6b991fadf3c9482ca59a247d9ded8f347783a9a7
TonyDaGroot <tony.nguyen@sendviper.io>
2019-01-16 23:47:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 function getFeed($feed_url) {
 
     $content = file_get_contents($feed_url);
-    $x = new SimpleXmlElement($content);
-
+    //$x = new SimpleXmlElement($content);
+    $x = jsondecode($content);
     echo "<ul>";
 
-    foreach($x->channel->item as $entry) {
+    foreach($x->data->recs as $entry) {
         echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></br>".$entry->description."</li>";
     }
     echo "</ul>";
 
 }
 
-getFeed('https://hnrss.org/newest');
+getFeed('http://downloadrss.test/feed.json');
 ?>
