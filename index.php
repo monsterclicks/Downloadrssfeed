b2fb3ca5f11af4dc5415e3d3ea8fdd300ebe6cc4
TonyDaGroot <tony.nguyen@sendviper.io>
2019-01-17 13:48:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 set_time_limit(0);
-//$cont = file_get_contents("https://api.blootle.com/topix_articles_get?count=5&bt_email=[-EMAILADDR-]&filter=%7B%22resource_type%22%3A%5B%22Offbeat%22%5D%7D");
+$cont = file_get_contents("https://api.blootle.com/topix_articles_get?count=5&bt_email=[-EMAILADDR-]&filter=%7B%22resource_type%22%3A%5B%22Offbeat%22%5D%7D");
 //file_put_contents("/tmp/tmp.txt",$cont);
-$newdata = file_get_contents("/tmp/tmp.txt");
-$newdata = str_replace("\\","",$newdata);
+//$newdata = file_get_contents("/tmp/tmp.txt");
+$newdata = str_replace("\\","",$cont);
 $data1 = json_decode($newdata, true);
 //$template = file_get_contents(__DIR__ .'test_template.html');
 //$totalc = count($data->data->recs);
 //$datatoarray = get_object_vars($data);
-var_dump($data);
+//var_dump($data);
 echo "=================";
 var_dump($data1);
 // for($i=0;$i<=$totalc;$i++){
